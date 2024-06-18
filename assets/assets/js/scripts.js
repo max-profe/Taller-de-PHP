e21b22420e861e9b8358eaee9174a1f12b266f2b
max-profe <ma.villalobosc@profesor.duoc.cl>
2024-06-18 18:51:57 -0400

    Update scripts.js

--- assets/assets/js/scripts.js
+++ assets/assets/js/scripts.js
@@ -1,1 +1,5 @@
-
+// Aquí puedes añadir cualquier funcionalidad adicional de JavaScript si es necesario
+document.addEventListener("DOMContentLoaded", function() {
+    // Ejemplo de funcionalidad JavaScript
+    console.log("Proyecto OnePage cargado correctamente.");
+});
