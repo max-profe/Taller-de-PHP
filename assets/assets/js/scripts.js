cee02bf4be5bc741d7d387eb2d08d217da47961f
max-profe <ma.villalobosc@profesor.duoc.cl>
2024-06-18 18:52:58 -0400

    Delete assets/assets/js directory

--- assets/assets/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// Aquí puedes añadir cualquier funcionalidad adicional de JavaScript si es necesario
-document.addEventListener("DOMContentLoaded", function() {
-    // Ejemplo de funcionalidad JavaScript
-    console.log("Proyecto OnePage cargado correctamente.");
-});
